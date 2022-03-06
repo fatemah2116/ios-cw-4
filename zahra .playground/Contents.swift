809c77db30bb2abdd57c4d5f4349d30d89f09ba7
fatamah <fatiabbasali2018@gmail.com>
2022-03-06 17:52:11 +0300

    fati66

--- zahra .playground/Contents.swift
+++ zahra .playground/Contents.swift
@@ -4,7 +4,7 @@ var greeting = "Hello, playground"
  let name = "fatamah zahra"
 
  var favouriteNumber = 6
-  
+var h = 1.75
 var hobby = "sewing"
 
-
+print(" hi my name is \(name) and my favouriteNumber is \(favouriteNumber) and my hobby is \(hobby) ")
